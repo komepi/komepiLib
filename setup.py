21d1d81157018fbc4f9cc99938b10b5636e1d85b
komepi <58720999+komepi@users.noreply.github.com>
2021-09-22 14:45:10 +0900

    前提モジュールにmatplotlibを追加

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name = "komepiLib",
-    version = "0.1",
+    version = "0.2",
     packages=find_packages(),
     extras_require={
-        "develop":["selenium"]
+        "develop":["selenium","matplotlib"]
     }
 )
