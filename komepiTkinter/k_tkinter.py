1adefcf2d7ae1a6b2fa01d2e0079692affd9c4f3
komepi <sterben_miyamiya_0921@i.softbank.jp>
2021-11-12 17:01:21 +0900

    memo

--- komepiTkinter/k_tkinter.py
+++ komepiTkinter/k_tkinter.py
@@ -88,7 +88,7 @@ def make_input_path(frame_r,  label_text, width_input, file_or_dir="file", initi
     IFileButton.pack(side=LEFT)
     return entry
 
-def make_button(frame_r, text, command, side = LEFT, **keyword):
+def make_button(frame_r, text, side, command, **keyword):
     ttk.Button(frame_r, text = text, command = lambda:command(**keyword)).pack(side=side)
 
 def make_label(frame_r, text,side="top"):
