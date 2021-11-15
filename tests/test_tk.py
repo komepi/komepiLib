94eeacaa553a883d349d56f01d6789782791cdb2
komepi <sterben_miyamiya_0921@i.softbank.jp>
2021-11-15 10:26:59 +0900

    テスト修正

--- tests/test_tk.py
+++ tests/test_tk.py
@@ -8,7 +8,7 @@ from komepiTkinter.k_tkinter import TreeView
 
 if __name__ == "__main__":
     win = tkm.new_window("500x500")
-    tree = TreeView(win,"left",["test0","test1","test2"],[200,200,200],12,True,False)
+    tree = TreeView(win,["test0","test1","test2"],[200,200,200],12,True,False)
     for i in range(20):
         iid = tree.insert_data(["data0","data1","data2"],is_open=True)
         tree.insert_data(["","data11","data12"],parent=iid)
